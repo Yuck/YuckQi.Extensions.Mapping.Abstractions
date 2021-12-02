f2565d368bfe48ba3e54d2f6806fc8921f0984a2
Kevin J Lambert <kevin.lambert@poconosystems.com>
2021-12-02 08:07:06 -0500

    First version release of mapper interface

---
+++ src/YuckQi.Extensions.Mapping.Abstractions/IMapper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace YuckQi.Extensions.Mapping.Abstractions
+{
+    public interface IMapper
+    {
+        object Map(object source, object destination, Type sourceType, Type destinationType);
+        object Map(object source, Type sourceType, Type destinationType);
+        TDestination Map<TDestination>(object source);
+        TDestination Map<TSource, TDestination>(TSource source);
+        TDestination Map<TSource, TDestination>(TSource source, TDestination destination);
+    }
+}
